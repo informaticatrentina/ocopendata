d2b3854e81d27fa7580189efe64602159ec5d419
Luca Realdi <lr@opencontent.it>
2016-02-26 17:07:12 +0100

    geonames da ini

--- classes/ckan/DatiTrentinoIt/Converter/OpenPA.php
+++ classes/ckan/DatiTrentinoIt/Converter/OpenPA.php
@@ -12,8 +12,6 @@ class OpenPA extends Converter
 {
     public static $geoUserName = 'lucarealdi';
 
-    public static $debug = false;
-
     protected function getExtras()
     {
         $extras = parent::getExtras();
@@ -21,89 +19,15 @@ class OpenPA extends Converter
             'key' => 'Generator',
             'value' => 'http://www.comunweb.it'
         );
-        $geoNameId = self::getGeoNamesId($this->getCustomField('Copertura Geografica'));
-        if ($geoNameId) {
+        $geoNames = \eZINI::instance('geonames.ini')->hasGroup('GeoNamesId');
+        $instanceId = \OpenPAInstance::current()->getIdentifier();
+        if (isset($geoNames[$instanceId])) {
             $extras[] = array(
                 'key' => 'Copertura Geografica URI',
-                'value' => 'http://www.geonames.org/' . $geoNameId
+                'value' => 'http://www.geonames.org/' . $geoNames[$instanceId]
             );
         }
 
         return $extras;
-    }
-
-    public static function getGeoNamesId($string)
-    {
-        if (!empty($string)) {
-            $geonameId = self::getStoredGeonameById( $string );
-            if (!$geonameId){
-                $searchUri = "http://api.geonames.org/search?username=" . self::$geoUserName . "&q=" . trim($string);
-                try {
-                    $data = \OpenPABase::getDataByURL( $searchUri );
-                    $searchXML = new SimpleXMLElement($data);
-                    if ((int)$searchXML->totalResultsCount > 0){
-                        foreach($searchXML->geoname as $geoname ){
-                            if ((string)$geoname->countryCode == 'IT' && (string)$geoname->fcode == 'ADM3'){
-
-                                $trans = \eZCharTransform::instance();
-                                $name = $trans->transformByGroup($geoname->name, 'urlalias');
-                                $stringTransformed = $trans->transformByGroup($string, 'urlalias');
-                                if ( $string == $name ){
-                                    if ( self::$debug ){
-                                        \eZCLI::instance()->output($string . ' -> ', false);
-                                        \eZCLI::instance()->output($geoname->name . ' ', false);
-                                        \eZCLI::instance()->output($geoname->geonameId);
-                                    }
-                                    $geonameId = (string)$geoname->geonameId;
-                                    self::getStoredGeonameById( $string, $geonameId );
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception $e) {
-                    $geonameId = null;
-                }
-            }
-            return $geonameId;
-        }
-        if ( self::$debug ){
-            \eZCLI::instance()->output($string . ' NOTFOUND', false);
-        }
-        return null;
-    }
-
-    protected static function getStoredGeonameById( $key, $value = null )
-    {
-        $data = array();
-
-        $siteData = eZSiteData::fetchByName( 'geonames_id' );
-        if ( !$siteData instanceof eZSiteData ){
-            $emptyData = json_encode(array());
-            eZDB::instance()->query( "INSERT INTO ezsite_data ( name, value ) values( 'geonames_id', '$emptyData' )" );
-            $siteData = eZSiteData::fetchByName( 'geonames_id' );
-            if ( self::$debug ){
-                \eZCLI::instance()->output('(new)', false);
-            }
-        }
-
-        if ( $siteData instanceof eZSiteData ){
-            $data = json_decode( $siteData->attribute('value'), 1);
-        }
-
-        if ( $value !== null ){
-            $data[] = array( $key => $value );
-            $jsonData = json_encode($data);
-            eZDB::instance()->query( "INSERT INTO ezsite_data ( name, value ) values( 'geonames_id', '$jsonData' )" );
-            $siteData = eZSiteData::fetchByName( 'geonames_id' );
-            $data = json_decode( $siteData->attribute('value'), 1);
-            if ( self::$debug ){
-                \eZCLI::instance()->output('(stored)', false);
-            }
-        }
-
-        if ( isset( $data[$key] ) )
-            return $data[$key];
-        return null;
     }
 }
