f0dce114bb7f7906d6e9b48b0a44678d38981694
OpenContent <info@opencontent.it>
2016-06-28 16:04:47 +0200

    aggiungo classi per schedulare l'import di oggetti recuperati con query API V2

---
+++ eventtypes/event/dataimportscheduler/dataimportschedulertype.php
@@ -0,0 +1,36 @@
+<?php
+
+class DataImportSchedulerType extends eZWorkflowEventType
+{
+  const WORKFLOW_TYPE_STRING = "dataimportscheduler";
+
+  function DataImportSchedulerType()
+    {
+      $this->eZWorkflowEventType( DataImportSchedulerType::WORKFLOW_TYPE_STRING, ezpI18n::tr( 'opencontent', 'Opendata import scheduler' ) );
+      $this->setTriggerTypes( array( 'content' => array( 'publish' => array( 'after' ) ) ) );
+    }
+    
+    function execute( $process, $event )
+    {
+        $parameterList = $process->attribute( 'parameter_list' );
+
+        $objectID = $parameterList['object_id'];
+        $object = eZContentObject::fetch( $objectID );
+        
+        if ( $object instanceof eZContentObject )
+        {
+          if ( $object->attribute( 'class_identifier' ) == 'data_import_scheduler' )
+          {
+              $importHandler = new DataImportSchedulerHandler();
+              $importHandler->scheduleImport( $object );
+          }
+        }
+        
+        return eZWorkflowType::STATUS_ACCEPTED;
+    }
+
+}
+
+eZWorkflowEventType::registerEventType( DataImportSchedulerType::WORKFLOW_TYPE_STRING, 'DataImportSchedulerType' );
+
+?>
---
+++ settings/sqliimport.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf-8"?
+
+[ImportSettings]
+AvailableSourceHandlers[]
+AvailableSourceHandlers[]=dataimportschedulerimporthandler
+
+[dataimportschedulerimporthandler-HandlerSettings]
+Enabled=true
+Name=Data Import Scheduler Import Handler
+ClassName=DataImportSchedulerImportHandler
+Debug=enabled
+
+*/ ?>
