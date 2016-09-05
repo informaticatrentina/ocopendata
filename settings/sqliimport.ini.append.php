bf1779f886ed5b3f747965f56921be4aaa50ba56
Luca Realdi <lr@opencontent.it>
2016-09-05 10:34:35 +0200

    fix sqliimport settings

--- settings/sqliimport.ini.append.php
+++ settings/sqliimport.ini.append.php
@@ -1,7 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [ImportSettings]
-AvailableSourceHandlers[]
 AvailableSourceHandlers[]=dataimportschedulerimporthandler
 
 [dataimportschedulerimporthandler-HandlerSettings]
