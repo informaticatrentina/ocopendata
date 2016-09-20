dfec970c87a5b6ed5079c36f90e85812b5bbd1cd
Luca Realdi <lr@opencontent.it>
2016-09-20 11:59:52 +0200

    prevent datatable js alert if data missing

--- classes/environments/datatable.php
+++ classes/environments/datatable.php
@@ -9,6 +9,28 @@ class DatatableEnvironmentSettings extends DefaultEnvironmentSettings
         \ArrayObject $query,
         \Opencontent\QueryLanguage\QueryBuilder $builder
     ) {
+
+        $parameters = $this->request->get;
+        $columns = $parameters['columns'];
+        $requestNames = array();
+        foreach( $columns as $index => $column ){
+            $requestNames[] = $column['name'];
+        }
+
+        $hits = array();
+        foreach($searchResults->searchHits as &$content){
+            $fixData = array();
+            foreach($content['data'] as $language => $data){
+                $diff = array_diff($requestNames, array_keys($data));
+                $missing = array_intersect($builder->fields, $diff);
+                if (!empty($missing)){
+                    $data = array_merge($data, array_fill_keys($missing, null));
+                }
+                $fixData[$language] = $data;
+                $content['data'] = $fixData;
+            }
+        }
+
         return array(
             'draw' => (int)( ++$this->request->get['draw']),
             'recordsTotal' => (int)$searchResults->totalCount,
